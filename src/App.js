d525a526cf899e87daff7955514967119ebf4b8b
gannon_smith <gansmith@umich.edu>
2023-06-08 02:48:01 -0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [nav, setNav] = useState(true);
+
+  useEffect(() => {
+
+  }, [nav])
+
+  function openNav() {
+    setNav(true)
+  }
+
+  function closeNav() {
+    setNav(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        { nav &&
+          <Nav />
+        }
+        <button onClick={openNav}>Open</button>
+        <button onClick={closeNav}>Close</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      
       </header>
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <header className="Nav-header">
+        <span>Nav</span>
+      </header>
+    </div>
+  );
+}
+
+export default Nav;
