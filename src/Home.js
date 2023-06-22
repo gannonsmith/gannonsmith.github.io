d4665458b11491abc39ca4324c06edc22983ba80
gannon_smith <gansmith@umich.edu>
2023-06-22 00:06:59 -0400

    adds basic template for homepage

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,13 @@ export default function Home() {
 
     return (
         <div className='home'>
-            <h1>Home</h1>
+            <div className='introduction'>
+                <h1>Hello, my name is <span className='bigger'>Gannon Smith</span></h1>
+                <h2>I'm a <span className='bigger'>Software Engineer</span></h2>
+            </div>
+            <div className='full-portrait-container'>
+                <img id="full-portrait" src="images/arv_photo.jpg" alt="full portrait"></img>
+            </div>
         </div>
     )
 }
