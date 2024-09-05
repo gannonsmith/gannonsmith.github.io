705e77001b21635d40f203587c1173f7300dbf6f
Gannon Smith <gansmith@umich.edu>
2024-09-04 20:32:47 -0400

    changing to nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="">
+        <div>
+
+        </div>
+        
+      </main>
+      <footer className="">
+        
+      </footer>
+    </div>
+  );
+}
